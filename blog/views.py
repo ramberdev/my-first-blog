796f640ca284271106a260b2ab7be5a2fb660a4e
ramberdev <ramberdev@gmail.com>
2021-11-21 20:18:49 +0500

    Modified templastes to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
+#подключение модели с models.py
+#подключение модели и шаблона html, также сортировка постов по дате публикации
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    #{}, — это место, куда мы можем добавить что-нибудь для использования в шаблоне
